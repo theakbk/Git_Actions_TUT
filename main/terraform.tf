58b049bfc966778bff29a63eba4742a02e38a531
theakbk <thea@kampmann.dk>
2022-11-15 15:35:09 +0100

    moved random block inside required providers block

--- main/terraform.tf
+++ main/terraform.tf
@@ -7,13 +7,15 @@ terraform {
             source = "hashicorp/azurerm"
             version = "~> 2.0"
         }
+    
+        random = {
+          source  = "hashicorp/random"
+          version = "=3.1.0"
+        }
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "=3.1.0"
-    }
     
+
     backend "azurerm" {
         key = "app.terraform.tfstate"
     }
