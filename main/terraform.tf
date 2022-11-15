ad23ba2bc62a0f8d22c365adc5fc2e6782756ab5
theakbk <thea@kampmann.dk>
2022-11-15 16:20:20 +0100

    azure version 4

--- main/terraform.tf
+++ main/terraform.tf
@@ -5,7 +5,7 @@ terraform {
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
-            version = "~> 2.84.0"
+            version = "~4"
         }
     
         random = {
