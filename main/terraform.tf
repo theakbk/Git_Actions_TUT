e44421f647cb78ac36c8d8939f639bdcc38d57c9
theakbk <thea@kampmann.dk>
2022-11-15 15:25:02 +0100

    add random provider block

--- main/terraform.tf
+++ main/terraform.tf
@@ -8,6 +8,12 @@ terraform {
             version = "~> 2.0"
         }
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+    
     backend "azurerm" {
         key = "app.terraform.tfstate"
     }
